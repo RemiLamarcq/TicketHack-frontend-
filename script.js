88d3419369dd4d68b2ce065b4ce881ea315280df
Blankaert <blankaertlouis@outlook.com>
2024-01-30 17:14:08 +0100

    modifier le font du texte

---
+++ script.js
@@ -0,0 +1,19 @@
+let departure = document.querySelector("#departure");
+let arrival = document.querySelector("#arrival");
+let btn = document.querySelector("#button");
+
+function changeCard() {
+  btn.addEventListener("click", () => {
+    console.log(departure.value);
+    console.log(arrival.value);
+    if (departure.value && arrival.value == "test") {
+      document.querySelector(".card2").innerHTML = `
+    <img id="train" src=${"../images/notfound.png"} alt="" height="200px">
+    <hr>
+    <p>${"No trip found."}</p>
+    `;
+    }
+  });
+}
+
+changeCard();
