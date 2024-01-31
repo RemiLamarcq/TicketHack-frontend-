7c1ccd1892702027b282f781b9c528059cc67544
Blankaert <blankaertlouis@outlook.com>
2024-01-31 10:58:23 +0100

    modifier le script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//const { response } = require("express");
+
 let departure = document.querySelector("#departure");
 let arrival = document.querySelector("#arrival");
 let btn = document.querySelector("#button");
@@ -6,14 +8,41 @@ function changeCard() {
   btn.addEventListener("click", () => {
     console.log(departure.value);
     console.log(arrival.value);
-    if (departure.value && arrival.value == "test") {
+    if (departure.value == "paris" && arrival.value == "lyon") {
       document.querySelector(".card2").innerHTML = `
-    <img id="train" src=${"./images/notfound.png"} alt="" height="200px">
-    <hr>
-    <p>${"No trip found."}</p>
+      <div id="cityCard">                   
+        <p>paris > lyon 16:23 126€ <button type="submit">Book</button></p>
+        </div>
+      </div>
+    `;
+    } else {
+      document.querySelector(".card2").innerHTML = `
+      <img id="train" src=${"./images/notfound.png"} alt="" height="200px">
+        <hr>
+        <p>${"No trip found."}</p>
     `;
     }
   });
 }
 
 changeCard();
+
+// document.querySelector("#button").addEventListener("click", function () {
+//   const departure = document.querySelector("#departure").value;
+//   const arrival = document.querySelector("#arrival").value;
+
+//   fetch("http://localhost:3000/trips", {
+//     method: "GET",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ departure, arrival }),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.result) {
+//         document.querySelector(".card2").innerHTML = `
+//         <div class="city">
+//           <p>${data.departure} > ${data.arrival} ${data.date} <button>book</button></p>
+//       `;
+//       }
+//     });
+// });
